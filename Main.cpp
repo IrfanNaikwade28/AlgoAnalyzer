1345d0cb6689a67019d4fb5d6a13f84e697a4ec7
IrfanNaikwade28 <106756605+IrfanNaikwade28@users.noreply.github.com>
2024-10-09 07:32:32 +0530

    Added MergeSort function with merge function

--- Main.cpp
+++ Main.cpp
@@ -100,6 +100,43 @@ void quickSort(int arr[], int low, int high)
         quickSort(arr, pi + 1, high);
     }
 }
+void merge(int arr[], int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; ++i)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; ++j)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+void mergeSort(int arr[], int left, int right)
+{
+    if (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
 
 int main()
 {
@@ -160,7 +197,9 @@ int main()
                 bubbleSort(arr, n);
                 break;
             case 4:
-                cout << "\nWorking";
+                cout << "\n-- Selected Algorithm: Merge Sort --\n";
+                mergeSort(arr, 0, n - 1);
+                printArray(arr, n);
                 break;
             case 5:
                 cout << "\nWorking";
